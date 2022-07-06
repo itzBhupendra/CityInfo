b0ca41a19044278406117bd6b4ef32bbd3c3ef9d
Bhupendra Singh <itzbhupendra@gmail.com>
2022-07-06 14:10:47 +0530

    Adding HTTPDelete

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -119,8 +119,25 @@ namespace CityInfo.API.Controllers
 
         }
 
+        [HttpDelete("{pointOfInterestId}")]
+        public ActionResult DeletePointOfInterest(int CityId,
+           int pointOfInterestId
+           )
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == CityId);
+            if (city == null)
+                return NotFound();
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(c => c.Id == pointOfInterestId);
+            if (pointOfInterestFromStore == null)
+                return NotFound();
+
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+            return NoContent();
+        }
 
 
 
-    }
+
+        }
 }
